9fd6e1c7b6b2bbe251664d9b6da5b3a99e94864e
vlad <qwasixqw@gmail.com>
2024-03-23 19:31:34 +0300

    manufacture where-buy

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,13 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 import initBurger from "./modules/burger-menu.js";
+import { popupMenuInit } from "./modules/popup-menu.js";
 
 // burger menu
 initBurger();
+
+// popupMenu
+popupMenuInit();
 
 // init Swiper:
 const swiper = new Swiper('.swiper', {
---
+++ src/js/modules/popup-menu.js
@@ -0,0 +1,20 @@
+const adaptiveItem = document.querySelectorAll('.adaptive-buy__item');
+
+function popupMenuInit() {
+	adaptiveItem.forEach((item) => {
+		const top = item.querySelector('.adaptive-buy__top');
+
+		top.addEventListener('click', () => {
+			popupHandler(item);
+		});
+	});
+}
+
+function popupHandler(item) {
+	item.classList.toggle('open');
+	const arrow = item.querySelector('.adaptive-buy__img img');
+	arrow.classList.toggle('rotate-up');
+	arrow.classList.toggle('rotate-down');
+}
+
+export { popupMenuInit };
